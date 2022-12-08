cde2d7bbd9353dd3343a5fe2b0906741900f9fb5
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-12-08 22:31:50 +0300

    for upload project

---
+++ Vendor/Vendor/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Vendor
+{
+    /**
+     *  торговый автомат
+     *  напитки (имя, обем) ,  -> горячий
+     *  интерфейс вендинг
+     */
+
+
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            UI uI = new UI();
+
+            uI.PushVendingDrinks();
+
+            uI.AskDrinks();
+            uI.AskDrinks();
+            uI.AskDrinks();
+            uI.AskDrinks();
+            uI.AskDrinks();
+        }
+    }
+}
